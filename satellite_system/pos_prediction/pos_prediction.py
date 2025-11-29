12a33aad6de5595bfb0e000d472b50e60ae87f60
Pavel <constonto2@gmail.com>
2025-11-29 20:16:07 +0300

    update pos_prediction

--- satellite_system/pos_prediction/pos_prediction.py
+++ satellite_system/pos_prediction/pos_prediction.py
@@ -7,14 +7,14 @@ class PosPrediction:
     def init(
         self,
         group: Group,
-        geom_info: Dict[int, GeoCoord]
+        positions: Dict[int, GeoCoord]
     ):
         self.group = group
-        self.geom_info = {}
+        self.positions = {}
 
     def predict_positions(self, target_time: datetime) -> Dict[int, Tuple(GeoCoord, float]):
         for orbit in self.group.orbits:
             new_longitude_asceding_node = (orbit.longitude_asceding_node - Constants.EarthAngularVelocityGrad * (target_time - orbit.creat_date) + 360) % 360
             for satellite in orbit.satellites:
                 coord = CoordConverter.phase_to_geo(new_longitude_asceding_node, satellite.phase, orbit.height))
-                self.geom_info[satellite.reg_number] = (coord, satellite.view_angle)
+                self.positions[satellite.reg_number] = (coord, satellite.view_angle)
