4dc7273b20a4b24825b77da0166ea52b9be08d3f
Pavel <constonto2@gmail.com>
2025-12-02 01:08:45 +0300

    Update static_coverage

--- satellite_system/coverage/static/static_coverage.py
+++ satellite_system/coverage/static/static_coverage.py
@@ -3,42 +3,63 @@ from ..pos_prediction.pos_prediction import PosPrediction
 from ..groups.group import Group
 from dataclasses import dataclass
 from ..utils.hex_manager import HexManager
-
+import numpy as np
+import math
+import ..coord_converter
+import ..utils.constants
 
 @dataclass
 class Aperture:
     center: GeoCoord
-    radius_km: float
+    aperture_angle: float
+
 
     def contains(self, point: GeoCoord) -> bool:
-        pass
+        x_point, y_point, z_point = CoordConverter().geo_to_decart(point)
+        x_center, y_center, z_center = CoordConverter().geo_to_decart(center)
+        point_vec = np.array([x_point, y_point, z_point])
+        center_vec = np.array([x_center, y_center, z_center])
+        return math.acos(np.dot(point_vec, center_vec) / (np.linalg.norm(center_vec) * np.linalg.norm(point_vec))) <= self.aperture_angle
 
+    @staticmethod
+    def calculate_aperture_angles_bulk(heights, view_angles):
+        view_angles_rad = np.radians(view_angles)
+        return 180 - view_angles_rad - np.degrees(
+            np.arcsin((1 + heights / Constants.EARTH_RADIUS) * np.sin(view_angles_rad))
+        )
 
 class StaticCoverage:
     def init(
         self,
         group: Group,
-        covered_centers : List[Tuple[float, float]]
+        target_time: datetime
+        covered_centers: Set[Tuple[float, float]]
     ):
         self.group = group
+        self.target_time = target_time
+        self.covered_centers = None
 
-def get_aperture_angle(
-    self,
-    coord: GeoCoord,
-    view_angle: float
-    ) -> float:
-    """Выдаёт угол раствора шарового сектора, что покрывается спутником."""
-    pass
 
-def get_covered_centers(self, coord: GeoCoord, view_angle: float]):-> set(Tuple[float, float])
-    pass
 
-def get_covered_centers_union(self, target_time: datetime):-> set(Tuple[float, float])
-    predictor = PosPrediction(group)
-    positions = predictor.predict_positions(target_time)
+def get_covered_centers(self, coord: GeoCoord, view_angle: float]) -> set(Tuple[float, float]):
+    if (self.covered_centers != None):
+        return self.covered_centers
+
+    view_angles = np.array([])
+    for orbit in self.group.orbits:
+        cur_view_angles = np.array([sat.view_angle for sat in orbit])
+        view_angles.concatenate(view_angles, cur_view_angles)
+    view_angles_rad = np.radians(view_angles)
+
+
+    apreture_angles = 180 - view_angles_rad - np.degrees(
+            np.arcsin((1 + heights / Constants.EARTH_RADIUS) * np.sin(view_angles_rad))
+        )
+def get_covered_centers_union(self):-> set(Tuple[float, float])
+
     for sat_reg_number, position in positions:
         covered_centers = get_covered_centers(position)
         covered_centers_union.update(covered_centers)
 
-def calculate_coverage(self, target_time: datetime):-> float
+def calculate_coverage(self, target_time: datetime) -> float:
     return len(get_covered_centers_union(self, target_time)) / HexManager().get_total_number()
