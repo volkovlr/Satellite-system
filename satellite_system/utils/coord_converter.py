059552a998fda09d476fe9f19f1560dd754505b3
Pavel <constonto2@gmail.com>
2025-12-02 01:01:13 +0300

    Update coord_converter

--- satellite_system/utils/coord_converter.py
+++ satellite_system/utils/coord_converter.py
@@ -1,40 +1,84 @@
 from .singleton import singleton
-from dataclasses import dataclass
-import math
+import numpy as np
+import numpy.typing as npt
+import .constants
 
 
-@dataclass
-class GeoCoord:
-    lat: float
-    lon: float
-    height: float
+GeoCoord_dtype = np.dtype([
+    ("lat", float),
+    ("lon", float),
+    ("height", float),
+])
 
+PhaseCoord_dtype = np.dtype([
+    ("longitude_asceding_node", float),
+    ("inclin", float),
+    ("phase_on_orbit", float),
+    ("height", float),
+])
 
-@dataclass
-class PhaseCoord:
-    longitude_asceding_node: float
-    inclin: float
-    phase_on_orbit: float
-    height: float
-
+DecartCoord_dtype = np.dtype([
+    ("x", float),
+    ("y", float),
+    ("z", float)
+])
 
 @singleton
 class CoordConverter:
-    def phase_to_geo(self, phase_coord: PhaseCoord) -> GeoCoord:
-        longitude_asceding_node, inclin, phase_on_orbit, height = phase_coord
-        inclin_rad = math.radians(inclin)
-        phase_on_orbit_rad = math.radians(phase_on_orbit)
+    @staticmethod
+    def phase_to_geo_np(phase_coord_np: npt.NDArray[PhaseCoord_dtype]) -> npt.NDArray[GeoCoord_dtype]:
+        longitude_asceding_nodes = phase_coord_np["longitude_asceding_node"]
+        inclins = phase_coord_np["inclin"]
+        phases_on_orbit = phase_coord_np["phase_on_orbit"]
+        heights = phase_coord_np["height"]
 
-        sin_lat = max(-1.0, min(1.0, math.sin(phase_on_orbit_rad) * math.sin(inclin_rad)))
-        lat_rad = math.asin(sin_lat)
-        lat = math.degrees(lat_rad)
+        longitude_asceding_nodes_rad = np.radians(longitude_asceding_nodes)
+        inclins_rad = np.radians(inclins)
+        phases_on_orbit_rad = np.radians(phases_on_orbit)
 
-        if inclin != 90:
-            lon = (longitude_asceding_node + math.degrees(math.atan2(
-                math.sin(phase_on_orbit_rad) * math.cos(inclin_rad),
-                math.cos(phase_on_orbit_rad)
-            ))) % 360
-        else:
-            lon = longitude_asceding_node
+        sin_lats = np.clip(
+            np.sin(phases_on_orbit_rad) * np.sin(inclins_rad),
+            -1.0, 1.0
+        )
 
-        return GeoCoord(lat, lon, height)
+        lats_rad = np.arcsin(sin_lats)
+        lats = np.degrees(lats_rad)
+
+        def formula(longitude_asceding_nodes: np.ndarray, phases_on_orbit_rad: np.ndarray, inclins_rad: np.ndarray) -> np.ndarray:
+            return (longitude_asceding_nodes + np.degrees(
+                np.arctan2(
+                    np.sin(phases_on_orbit_rad) * np.cos(inclins_rad),
+                    np.cos(phases_on_orbit_rad)
+                )
+            )) % 360
+
+        conditions = [phases_on_orbit_rad == 90, inclins_rad != 90, (0.5 * np.pi < phases_on_orbit_rad) & (phases_on_orbit_rad < 1.5 * np.pi), True]
+        choices = [0, formula(longitude_asceding_nodes_rad, phases_on_orbit_rad, inclins_rad), (longitude_asceding_nodes + 180) % 360, longitude_asceding_nodes]
+        lons = np.select(conditions, choices)
+
+        result = np.empty(len(lats), dtype=GeoCoord_dtype)
+        result["lat"] = lats
+        result["lon"] = lons
+        result["height"] = heights
+        return result
+
+
+    @staticmethod
+    def geo_to_dec_np(geo_coord_np: npt.NDArray[GeoCoord_dtype]) -> npt.NDArray[DecartCoord_dtype]:
+        lats = geo_coord_np["lat"]
+        lons = geo_coord_np["lon"]
+        heights = geo_coord_np["height"]
+
+        lats_rad = np.radians(lats)
+        lons_rad = np.radians(lons)
+
+        xs = (сonstants.EARTH_RADIUS + heights) * np.cos(lats_rad) * np.cos(lons_rad)
+        ys = (сonstants.EARTH_RADIUS + heights) * np.cos(lats_rad) * np.sin(lons_rad)
+        zs = (сonstants.EARTH_RADIUS + heights) * np.sin(lats_rad)
+
+        result = np.empty(len(lats), dtype=DecartCoord_dtype)
+        result["x"] = xs
+        result["y"] = ys
+        result["z"] = zs
+
+        return result
