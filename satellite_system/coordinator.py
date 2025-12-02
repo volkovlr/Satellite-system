0a5c6927fe871cb461707ebb81a52963023ade6f
Pavel <constonto2@gmail.com>
2025-12-02 23:21:28 +0300

    Update coordinator

--- satellite_system/coordinator.py
+++ satellite_system/coordinator.py
@@ -4,8 +4,9 @@ from satellite_system.groups.orbit import Orbit
 from satellite_system.groups.group_builder import GroupBuilder
 from .decorators import add_group_str
 from satellite_system.utils.logger import Logger
-
+import satellite_system.coverage.static.static_coverage as static_cov
 from typing import Dict
+import datetime
 
 from satellite_system.utils.singleton import singleton
 
@@ -17,7 +18,7 @@ class Coordinator:
       self.groups: Dict[str, Group] = {}
       self.orbits: Dict[str, Orbit] = {}
       self.satellites: Dict[str, Satellite] = {}
-
+      self.cache: Dict[(int, int), static_cov.StaticCoverageCalculator]
       self.logger = Logger()
 
   @add_group_str
@@ -47,3 +48,17 @@ class Coordinator:
           sat (Satellite)
       """
       self.satellites[sat.reg_number] = sat
+
+  def calculate_coverage(self, group: Group, resolution: int, target_time: datetime):
+      """Calculates coverage by group
+
+      Args:
+          group (Group)
+          resolution (int)
+      """
+      if (group.reg_number, resolution) in self.cache:
+          calculator = self.cache[(group.reg_number, resolution)]
+      else:
+          calculator = static_cov.StaticCoverageCalculator(group, resolution)
+          self.cache[(group.reg_number, resolution)] = calculator
+      return calculator.calculate_coverage(target_time)
