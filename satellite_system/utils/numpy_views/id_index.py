9ba7a6d8462d21da093994442f7efd8a937650bb
Pavel <constonto2@gmail.com>
2025-12-13 12:40:58 +0300

    Add id_index

---
+++ satellite_system/utils/numpy_views/id_index.py
@@ -0,0 +1,17 @@
+from typing import Dict, List
+
+class IdIndex:
+    """
+    Bijection between IDs and array indices.
+    """
+    def __init__(self, ids: List[int]):
+        self.id_to_idx: Dict[int, int] = {
+            id_: i for i, id_ in enumerate(ids)
+        }
+        self.idx_to_id: List[int] = ids
+
+    def idx(self, id_: int) -> int:
+        return self.id_to_idx[id_]
+
+    def id(self, idx: int) -> int:
+        return self.idx_to_id[idx]
