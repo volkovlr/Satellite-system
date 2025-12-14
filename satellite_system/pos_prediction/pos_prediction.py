2501531ef17c535ebbccfdc8fdf8a4ace26c69ae
Pavel <constonto2@gmail.com>
2025-12-14 05:37:21 +0300

    Update pos_prediction

--- satellite_system/pos_prediction/pos_prediction.py
+++ satellite_system/pos_prediction/pos_prediction.py
@@ -8,26 +8,27 @@ from ..utils.numpy_views.group_numpy_view import GroupNumpyView
 class PosPrediction:
     def __init__(self, group: Group):
         self.group = group
+        self.np_view = GroupNumpyView(self.group)
         self.cache = {}
 
     def predict_positions(self, target_time: datetime
                           ) -> dict[int, tuple[float, float, float]]:
+        target_time = np.datetime64(target_time, 'us')
         if target_time in self.cache:
             return self.cache[target_time]
 
-        np_view = GroupNumpyView(self.group)
-        time_delta = (target_time - np_view.orbit_view.creat_date).total_seconds()
+        time_delta = (target_time - self.np_view.orbits_view.creat_date) / np.timedelta64(1, 's')
         asc_lon_delta = - np.degrees(EARTH_ANGULAR_SPEED_RAD * time_delta)
-        asc_lon_new = ((np_view.orbit_view.longitude_asc + asc_lon_delta) + 360) % 360
-        sat_angular_speed_rad = np.sqrt((EARTH_MASS * G / (EARTH_RADIUS + np_view.orbit_view.height) ** 3))
+        asc_lon_new = ((self.np_view.orbits_view.longitude_asc + asc_lon_delta) + 360) % 360
+        sat_angular_speed_rad = np.sqrt((EARTH_MASS * G / (EARTH_RADIUS + self.np_view.orbits_view.height) ** 3))
         phase_delta = np.degrees(sat_angular_speed_rad * time_delta)
 
-        phase_delta = phase_delta[np_view.orbit_idx]
-        inclin = np_view.orbit_view.inclin[np_view.orbit_idx]
-        height = np_view.orbit_view.height[np_view.orbit_idx]
-        asc_lon_new = asc_lon_new[np_view.orbit_idx]
+        phase_delta = phase_delta[self.np_view.orbit_idx]
+        inclin = self.np_view.orbits_view.inclin[self.np_view.orbit_idx]
+        height = self.np_view.orbits_view.height[self.np_view.orbit_idx]
+        asc_lon_new = asc_lon_new[self.np_view.orbit_idx]
 
-        phase_new = np_view.sats_view.phases + phase_delta
+        phase_new = self.np_view.sats_view.phase + phase_delta
 
         phase_coords = np.empty((4, len(asc_lon_new)), dtype=np.float64)
         phase_coords[ASC_LON] = asc_lon_new
@@ -36,6 +37,9 @@ class PosPrediction:
         phase_coords[HEIGHT] = height
         geo_coords = CoordConverter().phase_to_geo_np(phase_coords)
 
-        result = (np_view.id_index.id(i) for i, coord in enumerate(geo_coords))
+        result = {
+            self.np_view.id_index.id(i): tuple(geo_coords[:, i])
+            for i in range(geo_coords.shape[1])
+        }
         self.cache[target_time] = result
         return result
